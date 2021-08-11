e31012464ee6cf5de22cade1de79ac435250c11b
rushithakare766 <70854157+rushithakare766@users.noreply.github.com>
2021-08-11 13:11:09 +0530

    Add files via upload

---
+++ gcdoftwonumbers.py
@@ -0,0 +1,5 @@
+# gcd of two numbers 
+import math
+a=int(input("Enter the number:")) # taking value from user
+b=int(input("Enter the number:")) # taking value from user
+print(math.gcd(a,b)) # print the value of  gcd of two number
